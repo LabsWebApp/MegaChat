a1c3f4979cd46c31ee08266d9a2a4cefd39c3db4
vovik <vovik@A6>
2026-02-17 15:13:43 +0300

    Каркас решения

---
+++ src/Fronts/MegaChat.Desktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MegaChat.Desktop.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Fronts/MegaChat.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MegaChat.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
